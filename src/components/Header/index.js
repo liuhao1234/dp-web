c1197da23f3d5546a18ac7ff160a54889f098e2c
liuhao1234 <728672190@qq.com>
2019-01-18 13:56:58 +0800

    添加退出功能

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,27 @@
 import React,{Component} from 'react';
-import { Input, Form,Modal, message } from 'antd';
-
+import logOut from '../../common/logOut';
+import { 
+	Input, 
+	Form,
+	Modal, 
+	message 
+} from 'antd';
+const { confirm } = Modal;
 class Header extends Component{
 	state = {
 		username : "管理员",
 		modalShow:false,
         usernameReal:""
+	};
+
+	handleLogout = ()=>{
+		confirm({
+		    title: '退出登录',
+		    content: '您确定要退出登录么？',
+		    onOk() {
+        		logOut();
+		    }
+  		})
 	};
 	
 	render() {
@@ -18,7 +34,7 @@ class Header extends Component{
 					<div className="right">
 						<span className="user"><small>您好！{this.state.usernameReal}</small></span>
 						<span><small>修改密码</small></span>
-						<span className="exit"><small>退出</small></span>
+						<span className="exit" onClick={this.handleLogout}><small>退出</small></span>
 						{/*<a href="/login" className="exit">退出</a>*/}
 					</div>
 				</div>
